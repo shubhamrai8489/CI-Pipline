5d1faa085577dde6d613da878cd375419adccb69
shubhamrai <shubhamrai8361@gmail.com>
2025-07-26 07:26:59 +0000

    CI pipline

--- test_main.py
+++ test_main.py
@@ -1,8 +1,7 @@
-import unittest 
-
+import unittest
 from main import add
 
 class TestMain(unittest.TestCase):
     def test_add(self):
-        assert add(2,3) == 5
-        assert add(-1,1) == 0
+        assert add(2, 3) == 5
+        assert add(-1, 1) == 0
